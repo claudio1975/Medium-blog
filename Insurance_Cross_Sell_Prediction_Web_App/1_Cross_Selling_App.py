ed07ecd1e482bdd48d67b5909d4cc2f3efe38bf8
claudio <34164395+claudio1975@users.noreply.github.com>
2023-05-01 11:06:46 +0200

    Add files via upload

---
+++ Insurance_Cross_Sell_Prediction_Web_App/1_Cross_Selling_App.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.write("# Welcome to the Insurance Cross-Selling Prediction Web App! 👋")
+
+st.markdown("""
+        **👈 Select a page from the dropdown on the left** to see some applications of the app!"""
+        )
+
+st.image("./images/cross_selling_homepage.jpg", width=500)
