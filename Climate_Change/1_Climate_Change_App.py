ab7fa984e86df67d630ef1a1d32c8042a416b804
claudio <34164395+claudio1975@users.noreply.github.com>
2023-11-04 08:17:54 +0100

    Update 1_Climate_Change_App.py

--- Climate_Change/1_Climate_Change_App.py
+++ Climate_Change/1_Climate_Change_App.py
@@ -1,12 +1,7 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import polars as pl
-from functime.cross_validation import train_test_split
-from functime.feature_extraction import add_fourier_terms
-from functime.forecasting import linear_model
-from functime.preprocessing import scale
-from functime.metrics import mase
+
 
 st.write("# Welcome to the Climate Change Dashboard! 👋")
 
