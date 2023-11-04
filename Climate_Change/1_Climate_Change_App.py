decf9b5554309878e13fe9509fc576bbb788feb6
claudio <34164395+claudio1975@users.noreply.github.com>
2023-11-04 08:04:53 +0100

    Update 1_Climate_Change_App.py

--- Climate_Change/1_Climate_Change_App.py
+++ Climate_Change/1_Climate_Change_App.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-st.write("# Welcome to the Climate Change Web App! 👋")
+st.write("# Welcome to the Climate Change Dashboard! 👋")
 
 st.markdown("""
         **👈 Select a page from the dropdown on the left** to see some applications of the app!"""
