f27d2ca1db84f0d685a40776dd1d73cb7f4a9d87
claudio <34164395+claudio1975@users.noreply.github.com>
2023-11-04 07:53:29 +0100

    Add files via upload

---
+++ Climate_Change/1_Climate_Change_App.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import functime as fc
+
+st.write("# Welcome to the Climate Change Web App! 👋")
+
+st.markdown("""
+        **👈 Select a page from the dropdown on the left** to see some applications of the app!"""
+        )
