23902ac7810eb52323f2aa0f2f7fc1e43a4f3aee
claudio1975 / claudio + giancaterino <c.giancaterino@gmail.com>
2025-05-01 15:39:36 +0200

    Initial commit of Stock_Market_Forecasting_CP project

---
+++ stockmarket.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.write("# Welcome to the StockMarket Forecasting Web App! 👋")
+
+st.markdown("""
+        **👈 Select a page from the dropdown on the left** to see some applications of the app!"""
+        )
+
+st.write('https://pixabay.com/')
+st.image("./images/stock_market.png", width=500)
+
+
+st.write("check out this [article](https://medium.com/@c.giancaterino/stock-market-forecasting-with-nixtla-conformal-prediction-49874285634e)")
+
+
+
